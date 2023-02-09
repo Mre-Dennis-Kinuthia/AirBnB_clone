f48548335fb656ca136d41bffcc19a806dae6ddd
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-02-09 13:56:36 +0000

    Updated file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+import os
 from models.base_model import BaseModel
 
+import json
 
 class FileStorage:
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
-    
+
+
     def all(self):
-        return self.__objects
-    
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
     def new(self, obj):
-        __objects.obj = BaseModel.id
-        
+        """sets in __objects the obj with key <obj class name>.id"""
+        obj_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_name, obj.id)] = obj
+
+    def save(self):
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    """def reload(self):
+        """"""reads the file and sets the self.__objects with the file content""""""
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)"""
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file exists)"""
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+            for key, value in obj_dict.items():
+                class_name = value.pop("__class__", None)
+                if class_name is not None:
+                    cls = eval(class_name)
+                    obj = cls(**value)
+                    FileStorage.__objects[key] = obj
