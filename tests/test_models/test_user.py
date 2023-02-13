a3bd5df7c843e1cec03ef8fe8390ebcf42bb466f
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-02-13 14:55:03 +0000

    init

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -13,18 +13,18 @@ class TestUser_instantiation(unittest.TestCase):
     def test_instance_type(self):
         """tests instantiation of User class"""
         self.assertEqual(User, type(User()))
-        
+
     def test_id_type(self):
         """tests id instantiation"""
         user_1 = User()
         user_2 = User()
         self.assertEqual(str, type(user_1.id))
         self.assertNotEqual(user_1.id, user_2.id)
-    
+
     def test_instance_storage(self):
         """tests storage  of User class instance"""
         self.assertIn(User(), models.storage.all().values())
-        
+
     def test_created_at_type(self):
         """tests created_at instantiation"""
         user_1 = User()
@@ -40,17 +40,17 @@ class TestUser_instantiation(unittest.TestCase):
         user_2 = User()
         self.assertEqual(datetime, type(user_1.updated_at))
         self.assertLess(user_1.updated_at, user_2.updated_at)
-        
+
     def test_email_password_instantiation(self):
         """tests email and password instantiation"""
         self.assertEqual(str, type(User().email))
         self.assertEqual(str, type(User().password))
-        
+
     def test_name_instantiation(self):
         """tests name instantiation"""
         self.assertEqual(str, type(User().first_name))
         self.assertEqual(str, type(User().last_name))
-     
+
     def test_str_representation(self):
         """tests if the string representation of the class User is correct"""
         user_1 = User()
@@ -60,7 +60,7 @@ class TestUser_instantiation(unittest.TestCase):
         bmstr = user_1.__str__()
         expectedstr = "[User] (12345) {{'id': '12345', 'created_at': '{}', 'updated_at': '{}', 'email': '', 'password': '', 'first_name': '', 'last_name': ''}}".format(date_today, date_today)
         self.assertEqual(bmstr, expectedstr)
-        
+
     def test_instantiation_kwargs(self):
         """Test that instantiation of class User with kwargs works"""
         date = datetime.today()
@@ -69,7 +69,7 @@ class TestUser_instantiation(unittest.TestCase):
         self.assertEqual(user_1.id, "12345")
         self.assertEqual(user_1.created_at, date)
         self.assertEqual(user_1.updated_at, date)
-        
+
     def test_instantiation_args_kwargs(self):
         """test that instantiation of class with argsdoesn't work while kwargs works"""
         date = datetime.today()
@@ -78,11 +78,8 @@ class TestUser_instantiation(unittest.TestCase):
         self.assertEqual(user_1.id, "12345")
         self.assertEqual(user_1.created_at, date)
         self.assertEqual(user_1.updated_at, date)
-        
+
     def test_instantiation_without_kwargs(self):
         """test that instantiation of class without kwargs None value won't work"""
         with self.assertRaises(TypeError):
             User(id=None, created_at=None, updated_at=None)
-               
-
-        
